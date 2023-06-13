e4257d22e60d0b8c15dfdef29c97aed30a8e904f
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-06-12 23:56:43 -0300

    Cambiar seccion Comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //Componentes
-import Comentarios from "./components/Comentarios"
+// import Comentarios from "./components/Comentarios"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Home from "./components/Home"
@@ -19,8 +19,9 @@ function App() {
         {/* Nuestra
         <Nuestra></Nuestra>
         */}
-     {/* Comentarios */}
+     {/* Comentarios 
      <Comentarios></Comentarios>
+     */}
     {/* Footer */}
       <Footer></Footer> 
    </>
