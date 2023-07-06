12b6e04dbad22f5cfa28b5e19cfa89765b69017b
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-07-05 23:54:49 -0300

    Agregar secciones

--- src/components/Nuestraapp.jsx
+++ src/components/Nuestraapp.jsx
@@ -11,55 +11,13 @@ function Nuestra() {
         <> 
          <div className={styles.sobre}>
           <h1>Nuestra App</h1>
-        </div> 
+        </div>  
         <div id="Nuestra" className={styles.contenedor}> 
           <div className={styles.caracteristicas}>
              <h1 className={styles.titulo}>Beneficios</h1>
-            <ul className={styles.lista}>
-             <li>
                <p className={styles.parrafo}>
                 Unete a una comunidad muy carismatica
-               </p> 
-             </li> 
-             <li>
-             <p className={styles.parrafo}>
-                Grandes empresas dedicadas a tus mascotas
-               </p> 
-             </li> 
-             <li> 
-             <p className={styles.parrafo}>
-                Una oportunidad de ayudar
-               </p> 
-             </li> 
-             <li>
-             <p className={styles.parrafo}>
-                Aumenta las ventas de tus negocios
-               </p> 
-             </li> 
-             <li>
-             <p className={styles.parrafo}>
-                Llega a nuevos publicos y amigos
-               </p> 
-             </li>  
-            </ul>
-            <ul className={styles.lista_res}>
-              <li>
-                <p>App Gratis</p>
-              </li>
-              <li>
-                <p>Comunidad Unida</p>
-              </li>
-              <li>
-                <p>Mascotas Contentas</p>
-              </li>
-              <li>
-                <p>No Te Lo Pierdas</p>
-              </li>
-           </ul>
-            <section className={styles.unirse}>
-              <button>Descargar Aqui</button>
-              <button>Siguiente</button>
-           </section>
+               </p>
           </div>
          <div className={styles.imagen}>
            <img src={img} /> 
@@ -68,6 +26,9 @@ function Nuestra() {
            <img src={imagen} /> 
          </div>
         </div> 
+        <div className={styles.seccionesapp}>
+
+        </div>
        </>
      
     )
